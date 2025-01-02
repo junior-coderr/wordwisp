20eb9b436d335b7afc169e66f0bfc9674685a6f2
Pratik Mishra <pratikmishra1833@gmail.com>
2025-01-03 00:06:03 +0530

    s

--- src/lib/google-storage.ts
+++ src/lib/google-storage.ts
@@ -4,7 +4,7 @@ import { Storage } from '@google-cloud/storage';
 const storage = new Storage({
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
     project_id: process.env.GOOGLE_PROJECT_ID
   }
 });
